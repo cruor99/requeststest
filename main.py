48449074614e1e16645c23993522ce9cb50fa364
cruor99 <kliknes@gmail.com>
2015-04-09 05:09:56 +0200

    I'm dumb

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 import requests
 
+
 class ReqRoot(BoxLayout):
     print requests.get("http://vg.no").headers
 
 
+class TestA(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(TestA, self).__init__(**kwargs)
+        print "it works"
+
+
+class TestB(BoxLayout):
+    pass
+
+
 class ReqApp(App):
+
     def on_pause(self):
         return True
 
